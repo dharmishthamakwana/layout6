b1b62cf416aeaa466ca34c0ba8fe01790b92e7d1
Admin <makwanadharmishtha44@gmail.com>
2023-01-18 10:26:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:pr9/screen1.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Screen6(),
+        //'s5': (context) => Screen5(),
+        //'s4': (context) => Screen4(),
+        //'s3':(context) => Screen3(),
+        //'s2':(context) => Screen2(),
+        //'/':(context) => Screen1(),
+      },
+    ),
+  );
+}
